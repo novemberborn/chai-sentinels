572e703165715ec70e52f34e590349c8a1245406
Mark Wubben <mark@novemberborn.net>
2014-03-18 21:49:29 +0000

    Generated documentation for 3d36490f4d4656c0a876560dbf837aa6760f896a

---
+++ toc.js
@@ -0,0 +1,156 @@
+window.files = [
+{
+  "path": "index.html",
+  "originalName": "README.md",
+  "originalPath": "README.md",
+  "name": "index.html",
+  "title": "chai-sentinels",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "chai-sentinels",
+      "title": "chai-sentinels"
+    },
+    {
+      "level": 2,
+      "slug": "installation",
+      "title": "Installation"
+    },
+    {
+      "level": 2,
+      "slug": "usage",
+      "title": "Usage"
+    }
+  ]
+},
+{
+  "path": "lib/main.js.html",
+  "originalName": "main.js",
+  "originalPath": "lib/main.js",
+  "name": "main.js.html",
+  "lang": "javascript",
+  "title": "main",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "main",
+      "title": "main"
+    },
+    {
+      "level": 2,
+      "slug": "usage",
+      "title": "Usage"
+    },
+    {
+      "level": 2,
+      "slug": "sentinel",
+      "title": "Sentinel"
+    },
+    {
+      "level": 2,
+      "slug": "static-sentinels",
+      "title": "Static sentinels"
+    },
+    {
+      "level": 2,
+      "slug": "stubobject-length-",
+      "title": "stubObject(length)"
+    },
+    {
+      "level": 2,
+      "slug": "stubarray-length-",
+      "title": "stubArray(length)"
+    }
+  ]
+},
+{
+  "path": "lib/Sentinel.js.html",
+  "originalName": "Sentinel.js",
+  "originalPath": "lib/Sentinel.js",
+  "name": "Sentinel.js.html",
+  "lang": "javascript",
+  "title": "Sentinel",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "sentinel",
+      "title": "Sentinel"
+    },
+    {
+      "level": 2,
+      "slug": "sentinel-label-propertiesobject-",
+      "title": "Sentinel(label, propertiesObject)"
+    },
+    {
+      "level": 2,
+      "slug": "sentinel-unary-arg-",
+      "title": "Sentinel.unary(arg)"
+    },
+    {
+      "level": 2,
+      "slug": "sentinel-inspect-",
+      "title": "Sentinel#inspect()"
+    }
+  ]
+},
+{
+  "path": "lib/assertions.js.html",
+  "originalName": "assertions.js",
+  "originalPath": "lib/assertions.js",
+  "name": "assertions.js.html",
+  "lang": "javascript",
+  "title": "Assertions",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "assertions",
+      "title": "Assertions"
+    },
+    {
+      "level": 2,
+      "slug": "expect-should",
+      "title": "Expect / Should"
+    },
+    {
+      "level": 3,
+      "slug": "-to-be-a-sentinel-to-not-be-a-sentinel",
+      "title": ".to.be.a.sentinel / .to.not.be.a.sentinel"
+    },
+    {
+      "level": 3,
+      "slug": "-to-be-matchingsentinels-expected-",
+      "title": ".to.be.matchingSentinels(expected)"
+    },
+    {
+      "level": 3,
+      "slug": "-to-not-be-matchingsentinels-expected-",
+      "title": ".to.not.be.matchingSentinels(expected)"
+    },
+    {
+      "level": 2,
+      "slug": "assert",
+      "title": "Assert"
+    },
+    {
+      "level": 3,
+      "slug": "issentinel-val-message-",
+      "title": "isSentinel(val, message)"
+    },
+    {
+      "level": 3,
+      "slug": "isnotsentinel-val-message-",
+      "title": "isNotSentinel(val, message)"
+    },
+    {
+      "level": 3,
+      "slug": "matchingsentinels-actual-expected-message-",
+      "title": "matchingSentinels(actual, expected, message)"
+    },
+    {
+      "level": 3,
+      "slug": "nonmatchingsentinels-actual-expected-message-",
+      "title": "nonMatchingSentinels(actual, expected, message)"
+    }
+  ]
+}
+];
